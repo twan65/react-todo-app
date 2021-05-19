f793a117919189db8ed7da9121768f044df327d4
t.an1 <t.an@allofthem.co.jp>
2021-05-19 19:11:26 +0900

    TODO追加機能

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
@@ -22,9 +22,28 @@ const App = () => {
     },
   ]);
 
+  // refを使用して変数に保存
+  // refを使う理由としてはIdはレンダリング対象ではないため
+  const nextId = useRef(4);
+
+  // コンポーネントの性能向上のためuseCallbackを使用
+  // propsで渡す関数の場合はuseCallbackを使った方が良い。
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1; // nextIdを1増やす
+    },
+    [todos],
+  );
+
   return (
     <TodoTemplate>
-      <TodoInsert />
+      <TodoInsert onInsert={onInsert} />
       <TodoList todos={todos} />
     </TodoTemplate>
   );
--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
+  // onSubmit使用理由はEnter押下時にもイベントを発生させるため。
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue('');
+
+      // submitイベントはブラウザの再読み込みを発生させるため、以下の関数を呼び出して防ぐ。
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
+
   return (
-    <form className="TodoInsert">
-      <input placeholder="TODOを入力してください" />
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        placeholder="TODOを入力してください"
+        value={value}
+        onChange={onChange}
+      />
       <button type="submit">
         <MdAdd />
       </button>
