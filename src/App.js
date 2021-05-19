2c3ae0901c72b00fab3667a119c6b873c77e55cd
t.an1 <t.an@allofthem.co.jp>
2021-05-19 18:47:36 +0900

    react-icons追加 データ表示機能追加

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 
 const App = () => {
-  return <TodoTemplate>Todo App을 만들자!</TodoTemplate>;
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'Test１',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: 'Test２',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: 'Test３',
+      checked: false,
+    },
+  ]);
+
+  return (
+    <TodoTemplate>
+      <TodoInsert />
+      <TodoList todos={todos} />
+    </TodoTemplate>
+  );
 };
 
 export default App;
